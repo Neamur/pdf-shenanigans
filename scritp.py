9fbd4d4eb2987f9aa3a2beb0abbecdf9447080b4
Neamur <rabbineamur20@gmail.com>
2025-07-20 17:01:46 +0400

    scritp.py handles KeyboardInterrupt gracefully

--- scritp.py
+++ scritp.py
@@ -1,7 +1,8 @@
 import os
-from pypdf import PdfMerger
+from pypdf import PdfWriter
 import inquirer
 from pprint import pprint
+import sys
 # pdfs = [a for a in os.listdir() if a.endswith(".pdf")]
 # #pdfs =['01.pdf','output.pdf' ]
 # merger = PdfMerger()
@@ -13,27 +14,41 @@ from pprint import pprint
 # merger.close()
 
 # print(pdfs)
+def main():
 
-questions = [
-    inquirer.Checkbox(
-        "pdfs",
-        message="Select the pdfs that you wish to merge (right arrow to select, left arrow to deselect):",
-        choices=[a for a in os.listdir() if a.endswith(".pdf")],
-    ),
-]
-    
-answers = inquirer.prompt(questions)
-pdfs =  [j for i in answers.values() for j in i]
-print(pdfs)
+    questions = [
+        inquirer.Checkbox(
+            "pdfs",
+            message="Select the pdfs that you wish to merge (right arrow to select, left arrow to deselect):",
+            choices=[a for a in os.listdir() if a.endswith(".pdf")],
+        ),
+    ]
+    # while True:
+        # try:
+    answers = inquirer.prompt(questions)
+    if answers is None:
+        sys.exit(0)
+    pdfs =  [j for i in answers.values() for j in i]
+    print(pdfs)
 
-merger = PdfMerger()
+    merger = PdfWriter()
 
-for pdf in pdfs:
-    merger.append(pdf)
+    for pdf in pdfs:
+        merger.append(pdf)
 
-merger.write("result.pdf")
-merger.close()
+    merger.write("result.pdf")
+    merger.close()
+    # break # ensures that the loop only runs once. since we didn't implement a exit option.
 
+        # except EOFError:
+            # print("this was maybe ctrl + d")
+            # sys.exit(0)
+        # except KeyboardInterrupt:
+            # print("this was maybe ctrl + c")
+            # sys.exit(0)
+
+if __name__ == "__main__":
+    main()
 
 ## by the 4B qwen
 # print([j for i in answers.values() for j in i]))
